cb7d2f287f0d620a3da214adf58a04210a89a948
Makoto Konishi <xiaoxishicheng@gmail.com>
2022-02-17 15:04:02 +0900

    userのモデルスペックをfactorybotに変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name { 'hoge' }
+    last_name { 'fuga' }
+    email { 'test@example.com' }
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,39 +3,43 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # 姓、名、メール、パスワードがあれば有効な状態であること
   it 'is valid with a first name, last name, email, and password' do
-    user = User.new(first_name: 'Adron', last_name: 'Sumner', email: 'tester@example.com', password: 'dottle-nouveau-pav')
+    user = FactoryBot.build(:user)
     expect(user).to be_valid
   end
 
   # 名がなければ無効な状態であること
   it 'is invalid without a first name' do
-    user = User.new(first_name: '')
+    user = FactoryBot.build(:user, first_name: '')
     user.valid? # validがfalse, またはinvalidがtrueでないとerrorsにバリデーションメッセージは格納されないので、valid?を明示している
     expect(user.errors[:first_name]).to include("can't be blank")
   end
+
   # 姓がなければ無効な状態であること
   it 'is invalid without a last name' do
-    user = User.new(last_name: '')
+    user = FactoryBot.build(:user, last_name: '')
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
+
   # メールアドレスがなければ無効な状態であること
   it 'is invalid without an email address' do
-    user = User.new(email: '')
+    user = FactoryBot.build(:user, email: '')
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
+
   # 重複したメールアドレスなら無効な状態であること
   it 'is invalid with a duplicate email address' do
-    User.create(first_name: 'hoge', last_name: 'fuga', email: 'hoge@example.com', password: 'password')
-    user = User.new(first_name: 'hoge', last_name: 'fuga', email: 'hoge@example.com', password: 'password')
+    FactoryBot.create(:user, email: 'hoge@example.com')
+    user = FactoryBot.build(:user, email: 'hoge@example.com')
     user.valid?
     expect(user.errors[:email]).to include('has already been taken')
   end
+
   # ユーザーのフルネームを文字列として返すこと
   it 'returns a full name as a string' do
-    user = User.new(first_name: 'hoge', last_name: 'fuga', email: 'hoge@example.com', password: 'password')
-    expect(user.name).to eq 'hoge fuga'
+    user = FactoryBot.build(:user)
+    expect(user.name).to eq "#{user.first_name} #{user.last_name}"
   end
 end
 
