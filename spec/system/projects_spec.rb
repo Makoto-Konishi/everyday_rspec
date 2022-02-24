6ff3087fe0b2b1bd902327e8055f3cd850ff503d
Makoto Konishi <xiaoxishicheng@gmail.com>
2022-02-24 15:59:14 +0900

    javaScriptを使った操作をテストする

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
 
   scenario 'user creates a new project' do
     user = FactoryBot.create(:user)
@@ -24,10 +21,5 @@ RSpec.describe "Projects", type: :system do
       expect(page).to have_content 'Test project'
       expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
-  end
-
-  scenario 'guest adds a project' do
-    visit projects_path
-    click_link 'New Project'
   end
 end
