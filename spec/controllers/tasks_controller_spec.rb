2cbf2de5995606699c62b26b02b8f6964b8aed96
Makoto Konishi <xiaoxishicheng@gmail.com>
2022-02-25 14:53:18 +0900

    shared_context （contextの共有）

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,38 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name: 'Test task')
-  end
+  include_context 'project setup'
 
   describe '#show' do
     it 'responds with JSON formatted output' do
-      sign_in @user
-      get :show, format: :json, params: { project_id: @project.id, id: @task.id }
+      sign_in user
+      get :show, format: :json, params: { project_id: project.id, id: task.id }
       expect(response.content_type).to include 'application/json'
     end
   end
 
   describe '#create' do
     it 'responds with JSON formatted output' do
-      sign_in @user
-      post :create, format: :json, params: { project_id: @project.id, task: { name: 'hoge'} }
+      sign_in user
+      post :create, format: :json, params: { project_id: project.id, task: { name: 'hoge'} }
       expect(response.content_type).to include 'application/json'
     end
 
     it 'adds a new task to the project' do
-      sign_in @user
+      sign_in user
       expect {
-        post :create, format: :json, params: { project_id: @project.id, task: { name: 'hoge'} }
-      }.to change(@project.tasks, :count).by(1)
+        post :create, format: :json, params: { project_id: project.id, task: { name: 'hoge'} }
+      }.to change(project.tasks, :count).by(1)
     end
 
     it 'requires authentication' do
       expect {
-        post :create, format: :json, params: { project_id: @project.id, task: { name: 'hoge'} }
-      }.not_to change(@project.tasks, :count)
+        post :create, format: :json, params: { project_id: project.id, task: { name: 'hoge'} }
+      }.not_to change(project.tasks, :count)
       expect(response).to_not be_successful
     end
   end
