e5d02171522671a1e58b8b6f1b872915f4ad234f
Makoto Konishi <xiaoxishicheng@gmail.com>
2022-02-25 17:47:29 +0900

    テストの可読性を改善するために、ヘルパーメソッドに処理を切り出す

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,25 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:project) { FactoryBot.create(:project, name: 'Test Project', owner: user) }
+  let!(:task) { project.tasks.create!(name: 'A Test Task Of Test Project') }
 
   scenario 'user toggles a task', js: true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, name: 'Test Project', owner: user)
-    task = project.tasks.create!(name: 'A Test Task Of Test Project')
+    sign_in_user(user)
+    go_to_project(project.name)
 
+    complete_task(task.name)
+    expect_complete_task(task.name)
+
+    undo_complete_task(task.name)
+    expect_incomplete_task(task.name)
+  end
+
+  def sign_in_user(user)
     visit root_path
     click_link 'Sign in'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
+  end
 
-    click_link 'Test Project'
-    check 'A Test Task Of Test Project'
-    expect(page).to have_css "label#task_#{task.id}.completed"
+  def go_to_project(name)
+    click_link name
+  end
+
+  def complete_task(name)
+    check name
+  end
+
+  def undo_complete_task(name)
+    uncheck name
+  end
+
+  def expect_complete_task(name)
+    expect(page).to have_css "label.completed", text: name
     expect(task.reload).to be_completed
+  end
 
-    uncheck 'A Test Task Of Test Project'
-    expect(page).to_not have_css "label#task_#{task.id}.completed"
+  def expect_incomplete_task(name)
+    expect(page).to_not have_css "label.completed", text: name
     expect(task.reload).to_not be_completed
   end
 end
